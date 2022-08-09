f553d4bbbea53100f7647410910edbc31e7b7e62
luisbboca <107447827+luisbboca@users.noreply.github.com>
2022-08-08 23:55:07 -0300

    Add files via upload

---
+++ arrays/arrays.js
@@ -0,0 +1,14 @@
+
+dias=[
+    {nro:1, dia:'LUNES'},
+    {nro:2, dia:'MARTES'},
+    {nro:3, dia:'MIERCOLES'},
+    {nro:4, dia:'JUEVES'},
+    {nro:5, dia:'VIERNES'},
+    {nro:6, dia:'SABADO'},
+    {nro:7, dia:'DOMINGO'}
+
+];
+dias.map(function(dias){
+    console.log(dias.nro+" => " +dias.dia);
+});
